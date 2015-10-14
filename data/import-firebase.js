db763ce07e73b58765ac7e2d09f17e670431f5e1
Uri Shaked <uri@salsa4fun.co.il>
2015-10-14 13:35:11 +0100

    fix firebase importer script

--- data/import-firebase.js
+++ data/import-firebase.js
@@ -2,7 +2,7 @@ var Firebase = require('firebase');
 var cats = require('./cats');
 
 var fbRef = new Firebase('https://topcat.firebaseio.com/cats');
-cats.getCatArray.forEach(function(cat) {
+cats.getCatArray().forEach(function(cat) {
 	fbRef.child(cat.id).set({
 		photo: cat.photo,
 		owner: cat.owner,
